8729ba6ebad81b99bc2ee443d163a9a46bc8ca2d
Igifigi <ignacyswiderski@wp.pl>
2023-04-25 14:04:44 +0200

    Create methods

--- xFlashMaker/Flashcard.cs
+++ xFlashMaker/Flashcard.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,15 @@ namespace xFlashMaker
     {
         private string term { get; set; }
         private string definition { get; set; }
+
+        public Flashcard(string term, string definition) {
+            this.term = term;
+            this.definition = definition;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", this.term, this.definition);
+        }
     }
 }
