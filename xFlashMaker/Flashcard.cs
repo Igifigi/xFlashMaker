451dd8370b4afd2922db1142866f767aa5b32bf1
Igifigi <ignacyswiderski@wp.pl>
2023-04-25 13:48:38 +0200

    Create Flashcard class

---
+++ xFlashMaker/Flashcard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xFlashMaker
+{
+    internal class Flashcard
+    {
+        private string term { get; set; }
+        private string definition { get; set; }
+    }
+}
